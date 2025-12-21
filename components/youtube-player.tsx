914209b5ad736d3a896a53953b05f61c82e1c966
bhavansri <srinathan.bhavan@gmail.com>
2025-12-21 18:12:06 -0500

    remove controls

--- components/youtube-player.tsx
+++ components/youtube-player.tsx
@@ -51,7 +51,7 @@ const YoutubePlayerComponent = forwardRef<YoutubePlayerRef, YoutubePlayerProps>(
       width: '100%',
       playerVars: {
         autoplay: 0,
-        controls: 1,
+        controls: 0,
         rel: 0,
         modestbranding: 1,
       },
